901d3abfcd46c0bb49d23146a35f04a821227cde
soojin2891 <ramizu@nate.com>
2025-05-20 15:23:36 +0900

    Update a.py

--- a.py
+++ a.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+plt.rcParams['font.family'] = 'Malgun Gothic'  # Windows
+# plt.rcParams['font.family'] = 'AppleGothic'  # Mac
+# plt.rcParams['font.family'] = 'NanumGothic'  # 리눅스용 설치 시
+plt.rcParams['axes.unicode_minus'] = False  # 음수 부호 깨짐 방지
 
 st.title("반도체 공정 시뮬레이터")
 
